6d024e62f64215145040760c06f133cba01f3638
AleksandrTan <rumych2013@gmail.com>
2021-05-12 11:28:13 +0300

    Start Admin

---
+++ mainsystem/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from mainsystem.views.startpage import MainView
+
+# ======================================= Main urls ==================================================================
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(success_url="homeadmin"), name='loginadmin'),
+    path('logoutadmin/', auth_views.LogoutView.as_view(), name='logoutadmin'),
+    path('', MainView.as_view(), name='homeadmin'),
+]
