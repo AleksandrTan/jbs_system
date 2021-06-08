86d5f8acd61ee206e1879a71f618dbbd5c64a074
alext <rumych2013@gmail.com>
2021-06-08 23:33:22 +0300

    home -1

--- mainsystem/views/proxy/update_proxy.py
+++ mainsystem/views/proxy/update_proxy.py
@@ -9,9 +9,10 @@ class UpdateProxy(APIView):
     Issue updated proxy
     """
     def get(self, request, *args, **kwargs):
-        print(request, request.GET, args, kwargs)
         data_response = dict()
+        print(request.GET)
         proxy = ProxyWork.get_proxy_update(request.GET["proxy_id"])
+        print(proxy, 4000)
         if proxy:
             data_response["status"] = True
             data_response["proxy"] = proxy[1]
@@ -19,7 +20,7 @@ class UpdateProxy(APIView):
         else:
             data_response["status"] = False
         # update proxy fail count
-        if request.GET["proxy_id"] != '0':
+        if request.GET["fail"] == "True":
             ProxyWork.update_request_fail(request.GET["proxy_id"])
 
         return Response(data_response)
