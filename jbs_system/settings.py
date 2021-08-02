9598768cc018e1e70fc02747afb1759daee98181
AleksandrTan <rumych2013@gmail.com>
2021-08-02 15:14:47 +0300

    Test -1

--- jbs_system/settings.py
+++ jbs_system/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'mainsystem.context_processors.main_data'
+                # 'mainsystem.context_processors.main_data'
             ],
         },
     },
